77d2b16dd21cde7d7c5f560bc3d9870f02cc5f9b
Александр Рукавишников <as.rukavishnikov@mail.ru>
2024-03-28 15:45:35 +0500

    added show/hide password eye to fields with password type

--- frontend/src/components/shared/input/Input.tsx
+++ frontend/src/components/shared/input/Input.tsx
@@ -1,14 +1,35 @@
-import React, { forwardRef } from "react";
+"use client";
+
+import React, { forwardRef, useState } from "react";
 import styles from "./input.module.scss";
+import Image from "next/image";
 
 type InputProps = React.ComponentProps<"input"> & {};
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ children, ...props }: InputProps, ref) => {
+  ({ children, type, ...props }: InputProps, ref) => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
     return (
-      <input className={styles.wrapper} ref={ref} {...props}>
-        {children}
-      </input>
+      <div className={styles.wrapper}>
+        <input
+          className={styles.input}
+          maxLength={type === "password" ? 22 : 30}
+          type={type === "password" && !passwordVisible ? "password" : "text"}
+          ref={ref}
+          {...props}
+        />
+        {type === "password" && (
+          <Image
+            className={styles.eye}
+            src={"/PasswordEye.svg"}
+            alt={"Показать/скрыть пароль"}
+            width={24}
+            height={24}
+            onClick={() => setPasswordVisible((prev) => !prev)}
+          />
+        )}
+      </div>
     );
   },
 );
