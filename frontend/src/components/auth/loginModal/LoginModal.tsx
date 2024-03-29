89efd867670b17b48bbc63874986b931d207dd6d
Александр Рукавишников <as.rukavishnikov@mail.ru>
2024-03-30 00:45:34 +0500

    login modal fix

--- frontend/src/components/auth/loginModal/LoginModal.tsx
+++ frontend/src/components/auth/loginModal/LoginModal.tsx
@@ -1,85 +1,15 @@
-"use client";
-
 import React from "react";
 import Modal from "@/components/shared/modal/Modal";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import Input from "@/components/shared/controls/input/Input";
-import Button from "@/components/shared/controls/button/Button";
-import styles from "./loginModal.module.scss";
+import LoginForm from "@/components/auth/loginModal/components/loginForm/LoginForm";
 
 type LoginModalProps = {};
 
-const loginSchema = z.object({
-  email: z
-    .string()
-    .email("Необходимо ввести почту")
-    .min(1, "Необходимо ввести почту"),
-  password: z.string().min(8, "Пароль должен быть больше 8 символов"),
-});
-
-type LoginSchema = z.infer<typeof loginSchema>;
-
 const LoginModal = (props: LoginModalProps) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { isValid, errors, isSubmitting },
-  } = useForm<LoginSchema>({
-    resolver: zodResolver(loginSchema),
-    mode: "onTouched",
-  });
-
-  const onSubmit = (data: LoginSchema) => {
-    console.log(data);
-  };
-
   return (
     <Modal state={"login"}>
       <Modal.Window>
         <Modal.Title>Вход в аккаунт</Modal.Title>
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-          <div className={styles.inputs}>
-            <Input
-              type={"email"}
-              placeholder={"Введите email"}
-              {...register("email")}
-            />
-            {!!errors.email && (
-              <p className={styles.error}>{errors.email.message}</p>
-            )}
-            <Input
-              type={"password"}
-              placeholder={"Введите пароль"}
-              {...register("password")}
-            />
-            {!!errors.password && (
-              <p className={styles.error}>{errors.password.message}</p>
-            )}
-          </div>
-
-          <div className={styles.btns}>
-            <Button
-              btnType={"button"}
-              fontSize={"small"}
-              font="comfortaa"
-              style="filled"
-              disabled={!isValid || isSubmitting}
-            >
-              Войти
-            </Button>
-            <Button
-              btnType={"link"}
-              fontSize={"small"}
-              href={"?state=register"}
-              font="comfortaa"
-              style="flat"
-            >
-              Зарегистрироваться
-            </Button>
-          </div>
-        </form>
+        <LoginForm />
       </Modal.Window>
     </Modal>
   );
---
+++ frontend/src/components/auth/loginModal/components/loginForm/LoginForm.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Input from "@/components/shared/controls/input/Input";
+import Button from "@/components/shared/controls/button/Button";
+import styles from "./loginForm.module.scss";
+
+const loginSchema = z.object({
+  email: z
+    .string()
+    .email("Необходимо ввести почту")
+    .min(1, "Необходимо ввести почту"),
+  password: z.string().min(8, "Пароль должен быть больше 8 символов"),
+});
+
+type LoginSchema = z.infer<typeof loginSchema>;
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid, errors, isSubmitting },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = (data: LoginSchema) => {
+    console.log(data);
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.inputs}>
+        <Input
+          type={"email"}
+          placeholder={"Введите email"}
+          {...register("email")}
+        />
+        {!!errors.email && (
+          <p className={styles.error}>{errors.email.message}</p>
+        )}
+        <Input
+          type={"password"}
+          placeholder={"Введите пароль"}
+          {...register("password")}
+        />
+        {!!errors.password && (
+          <p className={styles.error}>{errors.password.message}</p>
+        )}
+      </div>
+
+      <div className={styles.btns}>
+        <Button
+          btnType={"button"}
+          fontSize={"small"}
+          font="comfortaa"
+          style="filled"
+          disabled={!isValid || isSubmitting}
+        >
+          Войти
+        </Button>
+        <Button
+          btnType={"link"}
+          fontSize={"small"}
+          href={"?state=register"}
+          font="comfortaa"
+          style="flat"
+        >
+          Зарегистрироваться
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
