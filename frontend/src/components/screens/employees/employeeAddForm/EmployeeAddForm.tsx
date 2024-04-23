00c8ab19ef1f1cdc299103343377fb3d224bc85f
Александр Рукавишников <as.rukavishnikov@mail.ru>
2024-04-23 17:15:38 +0500

    added error messages to employee creation

--- frontend/src/components/screens/employees/employeeAddForm/EmployeeAddForm.tsx
+++ frontend/src/components/screens/employees/employeeAddForm/EmployeeAddForm.tsx
@@ -10,6 +10,7 @@ import Button from "@/components/shared/controls/button/Button";
 import EmployeeService from "@/services/employees/EmployeeService";
 import { useSession } from "next-auth/react";
 import toast from "react-hot-toast";
+import InputError from "@/components/shared/inputError/InputError";
 
 const employeeAddFormSchema = z.object({
   full_name: z.string().min(1, "Необходимо ввести имя"),
@@ -25,6 +26,7 @@ const EmployeeAddForm = () => {
     register,
     formState: { errors, isValid, isSubmitting },
     handleSubmit,
+    reset,
   } = useForm<EmployeeAddFormSchema>({
     mode: "onTouched",
     resolver: zodResolver(employeeAddFormSchema),
@@ -34,10 +36,12 @@ const EmployeeAddForm = () => {
     if (!session?.user?.currentRestaurant) return;
 
     await EmployeeService.create(session.user.currentRestaurant, {
-      user: data
+      user: data,
     }).then(() => {
-      toast.success ("Сотрудник успешно добавлен");
+      toast.success("Сотрудник успешно добавлен");
     });
+
+    reset();
   };
 
   return (
@@ -49,19 +53,26 @@ const EmployeeAddForm = () => {
         placeholder={"Имя сотрудника"}
         {...register("full_name")}
       />
+      <InputError error={errors.full_name?.message} className={styles.error} />
       <div className={styles.loginContainer}>
-        <Input
-          inputStyle={"alternative"}
-          type={"text"}
-          placeholder={"Почта"}
-          {...register("email")}
-        />
-        <Input
-          inputStyle={"alternative"}
-          type={"password"}
-          placeholder={"Пароль"}
-          {...register("password")}
-        />
+        <div>
+          <Input
+            inputStyle={"alternative"}
+            type={"text"}
+            placeholder={"Почта"}
+            {...register("email")}
+          />
+          <InputError error={errors.email?.message} className={styles.error} />
+        </div>
+        <div>
+          <Input
+            inputStyle={"alternative"}
+            type={"password"}
+            placeholder={"Пароль"}
+            {...register("password")}
+          />
+          <InputError error={errors.password?.message} className={styles.error} />
+        </div>
       </div>
       <Button
         btnType={"button"}
